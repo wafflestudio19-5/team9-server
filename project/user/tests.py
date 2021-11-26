8bb158941760e41e8f4ff922195d21829f9f6afd
IHIHHIHI <yiminjune@snu.ac.kr>
2021-11-27 00:32:15 +0900

    add imports in test

--- project/user/tests.py
+++ project/user/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from factory.django import DjangoModelFactory
+from user.models import User
+from user.serializers import jwt_token_of
+from rest_framework import status
+import json
 
 # Create your tests here.
