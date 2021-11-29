62c02f2d30b9034d5c394b9863fb14b115ff63c5
leejoey0921 <86173819+leejoey0921@users.noreply.github.com>
2021-11-29 21:13:23 +0900

    black으로 코드 포맷

--- project/config/urls.py
+++ project/config/urls.py
@@ -26,20 +26,24 @@ from drf_yasg.views import get_schema_view
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Waffle Team-9 API",
-      default_version='v1',
-      description="Facebook toy project",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Waffle Team-9 API",
+        default_version="v1",
+        description="Facebook toy project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("user.urls")),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 
 
