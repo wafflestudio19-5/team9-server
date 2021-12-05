6771a5cd467da9e10951acc5fcb0956612e358c1
IHIHHIHI <yiminjune@snu.ac.kr>
2021-12-06 01:11:16 +0900

    feat: add url

--- project/newsfeed/urls.py
+++ project/newsfeed/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path, include
-from .views import PostViewSet
+from .views import PostViewSet, LikeViewSet
 from rest_framework.routers import SimpleRouter
 from django.conf import settings
 from django.conf.urls.static import static
 
-router = SimpleRouter()
-router.register("newsfeed", PostViewSet, basename="PostList")  # /api/v1/newsfeed/
+postRouter = SimpleRouter()
+postRouter.register("newsfeed", PostViewSet, basename="PostList")  # /api/v1/newsfeed/
+likeRouter = SimpleRouter()
+likeRouter.register("like", LikeViewSet, basename="LikeList")  # /api/v1/like/
 
 urlpatterns = [
-    path("", include(router.urls), name="PostList"),
+    path("", include(postRouter.urls), name="PostList"),
+    path("", include(likeRouter.urls), name="Likelist"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
