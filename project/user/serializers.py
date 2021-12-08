ae873f7faf984400f91a54775e4929853e96b6a9
leejoey0921 <86173819+leejoey0921@users.noreply.github.com>
2021-12-09 00:44:09 +0900

    user/serializers.py 수정

--- project/user/serializers.py
+++ project/user/serializers.py
@@ -81,7 +81,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # Django 기본 User 모델에 존재하는 필드 중 일부
-        fields = ()
+        fields = ("id", "email")
         extra_kwargs = {"password": {"write_only": True}}
 
     def validate(self, data):
