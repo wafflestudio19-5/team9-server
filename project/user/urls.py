5157e73c92cdd9bd4b4f5b3523ad51c7e50b0e10
leejoey0921 <86173819+leejoey0921@users.noreply.github.com>
2021-12-16 03:34:53 +0900

    Update urls.py

--- project/user/urls.py
+++ project/user/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("signup/", UserSignUpView.as_view(), name="signup"),  # /api/v1/signup/
     path("login/", UserLoginView.as_view(), name="login"),  # /api/v1/login/
-    path("kakao/", KakaoView.as_view(), name="kakao_login"),
-    path("kakao/callback", KakaoCallbackView.as_view(), name="kakao_callback"),
+    path("kakao/", KakaoView.as_view(), name="kakao_login"),  # /api/v1/kakao/
+    path("kakao/callback/", KakaoCallbackView.as_view(), name="kakao_callback"),  # /api/v1/kakao/callback/
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
