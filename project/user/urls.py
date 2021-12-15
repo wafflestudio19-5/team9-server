4bd1ab937b30567a1772831ae4d77afa0e51d20f
leejoey0921 <86173819+leejoey0921@users.noreply.github.com>
2021-12-16 02:50:57 +0900

    url path 추가

--- project/user/urls.py
+++ project/user/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from .views import UserLoginView, UserSignUpView
+from .views import UserLoginView, UserSignUpView, KakaoView, KakaoCallbackView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("signup/", UserSignUpView.as_view(), name="signup"),  # /api/v1/signup/
     path("login/", UserLoginView.as_view(), name="login"),  # /api/v1/login/
+    path("kakao/", KakaoView.as_view(), name="kakao_login"),
+    path("kakao/callback", KakaoCallbackView.as_view(), name="kakao_callback"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
