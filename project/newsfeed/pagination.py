10afe6cfca9cf5a8d340f124496008d60920f128
leejoey0921 <86173819+leejoey0921@users.noreply.github.com>
2021-12-28 00:29:13 +0900

    불필요한 paginator 삭제

--- project/newsfeed/pagination.py
+++ project/newsfeed/pagination.py
@@ -1,12 +1,6 @@
 from rest_framework.pagination import CursorPagination
 
 
-class CustomPagination(CursorPagination):
-    page_size = 20
-    ordering = "-created_at"
-    cursor_query_param = "c"
-
-
 class CommentPagination(CursorPagination):
     page_size = 20
     ordering = "created"
