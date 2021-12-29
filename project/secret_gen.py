edb6edc296c5fcecbd968466ffdbdcbe9b2624ab
jaejae2374 <jaejae2374@snu.ac.kr>
2021-12-29 14:32:15 +0900

    fix/credentials problem

--- project/secret_gen.py
+++ project/secret_gen.py
@@ -3,15 +3,11 @@ import secrets
 import os
 from django.core.management.utils import get_random_secret_key
 
-print("###########################")
-print(os.environ.get("S3_SECRET_KEY"))
-print("###########################")
-# S3_SECRET_KEY
 f = open("secrets.json", "w")
 data = {
     "SECRET_KEY": get_random_secret_key(),
     "KAKAO_APP_KEY": secrets.token_hex(32),
-    "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY"),
-    "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID"),
+    "AWS_SECRET_ACCESS_KEY": "",
+    "AWS_ACCESS_KEY_ID": "",
 }
 f.write(json.dumps(data))
