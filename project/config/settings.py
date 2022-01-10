da3fe860a6b531a8ffacb1887dc71ea3e1b3a078
jaejae2374 <jaejae2374@snu.ac.kr>
2022-01-10 23:35:59 +0900

    secrets.py에 get_secret

--- project/config/settings.py
+++ project/config/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "HOST": "database-careerly.cq3boo3w9eua.ap-northeast-2.rds.amazonaws.com",
+        "HOST": get_secret("DATABASE_HOST"),
         "PORT": 3306,
         "NAME": "toy_project",  # database name 변경
         "USER": "admin",
