269be712ac9b45e4cfcbad2d0ffff60d9a25fa17
IHIHHIHI <yiminjune@snu.ac.kr>
2022-01-16 22:42:26 +0900

    feat: add pagination

--- project/user/pagination.py
+++ project/user/pagination.py
@@ -1,5 +1,9 @@
-from rest_framework.pagination import CursorPagination
+from rest_framework.pagination import CursorPagination, LimitOffsetPagination
 
 
 class UserPagination(CursorPagination):
     ordering = "date_joined"
+
+
+class FriendPagination(LimitOffsetPagination):
+    offset_query_param = "cursor"
