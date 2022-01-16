bcadaa3f9c3d3f1177641850c3c74edfb75c1df6
IHIHHIHI <yiminjune@snu.ac.kr>
2022-01-16 21:42:21 +0900

    feat: add message

--- project/user/utils.py
+++ project/user/utils.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from datetime import datetime
+import six
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 def jwt_get_secret_key(user_model):
@@ -20,10 +22,6 @@ def validate_birth(birth):
         raise ValidationError(_("생일이 현재 시간보다 나중일 수는 없습니다."), params={"birth": birth})
 
 
-import six
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
-
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (six.text_type(user.pk) + six.text_type(timestamp)) + six.text_type(
@@ -32,3 +30,7 @@ class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
 
 
 account_activation_token = AccountActivationTokenGenerator()
+
+
+def message(domain, uidb64, token):
+    return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n회원가입 링크 : http://{domain}/account/activate/{uidb64}/{token}\n\n감사합니다."
