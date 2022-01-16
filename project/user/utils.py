74f72f178770f5b2e69eff7d277977a2d0db6f5f
IHIHHIHI <yiminjune@snu.ac.kr>
2022-01-16 17:54:58 +0900

    feat: add token generator

--- project/user/utils.py
+++ project/user/utils.py
@@ -18,3 +18,17 @@ def validate_gender(gender):
 def validate_birth(birth):
     if birth > datetime.now().date():
         raise ValidationError(_("생일이 현재 시간보다 나중일 수는 없습니다."), params={"birth": birth})
+
+
+import six
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (six.text_type(user.pk) + six.text_type(timestamp)) + six.text_type(
+            user.is_active
+        )
+
+
+account_activation_token = AccountActivationTokenGenerator()
