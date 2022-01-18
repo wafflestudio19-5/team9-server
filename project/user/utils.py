d20ec24e924c4f77f939e4cfc8878d5f3caafc58
IHIHHIHI <yiminjune@snu.ac.kr>
2022-01-18 19:26:36 +0900

    fix: change message url

--- project/user/utils.py
+++ project/user/utils.py
@@ -33,4 +33,4 @@ account_activation_token = AccountActivationTokenGenerator()
 
 
 def message(domain, uidb64, token):
-    return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n회원가입 링크 : http://{domain}/account/activate/{uidb64}/{token}\n\n감사합니다."
+    return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n회원가입 링크 : http://{domain}/api/v1/account/activate/{uidb64}/{token}/\n\n감사합니다."
